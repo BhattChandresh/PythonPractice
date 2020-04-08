2f078089440dfbcf16974bd065fd4e1e69f4f24d
Chandresh Bhatt <33648618+BhattChandresh@users.noreply.github.com>
2020-04-08 12:57:48 +0530

    Add files via upload
    import, from demo

---
+++ AreaOfCircle.py
@@ -0,0 +1,20 @@
+# Reference : Durga Software Solutions.
+
+# Example-1
+
+from math import pi
+r = int(input('Enter a radius:'))
+area = pi*r**2
+print('Area of Cirle is : ', area)
+
+# Example-2
+import math
+r = int(input('Enter a radius:'))
+area = math.pi*r**2
+print('Area of Cirle is : ', area)
+
+# Example-3
+import math as m
+r = int(input('Enter a radius:'))
+area = m.pi*r**2
+print('Area of Cirle is : ', area)
